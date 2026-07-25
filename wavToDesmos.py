1f7005fa578b19ea65a1d216dc4157f724a50a4b
dannium <dannium161@gmail.com>
2026-07-25 00:56:58 -0400

    print stuff
    made user friendly

--- wavToDesmos.py
+++ wavToDesmos.py
@@ -3,9 +3,13 @@ import struct
 import numpy as np
 import matplotlib.pyplot as plt
 
+#returns first half of list
 def half(list):
     return list[:len(list)//2]
 
+print("go to https://www.desmos.com/calculator/yccglkqkej to get the template\n\n")
+
+#change "theaudio.wav" to any .wav file to read (has to be in the same directory as this script)
 with wave.open("theaudio.wav", "rb") as audio:
     print("Converting data...")
     frameRate = audio.getframerate() #amount of frames per second
@@ -34,14 +38,16 @@ with wave.open("theaudio.wav", "rb") as audio:
     ######################################## convert with fourier
     desmosData = []
 
-    fps = frameRate//int(input("How many frames do you want to play in desmos each second?\n"))
+    fps = frameRate//int(input("How many frames do you want to play in desmos each second? (Recommended ~10)\n"))
     totalFrames = len(data)//fps
 
 
-    layerNum = 300
+    layerNum = int(input("How many layers do you want? (Recommended ~150 for short (5s), ~40 for long (30s+))\n"))
+
     for i in range(layerNum):
         desmosData.append("b_{" + str(i) + "} = [")
 
+    print("formatting data to desmos...")
     for frameNum in range(totalFrames):
         dataChunk = data[frameNum*fps:(frameNum+1)*fps]  
         #freqs (x)
@@ -49,28 +55,39 @@ with wave.open("theaudio.wav", "rb") as audio:
         #amp of freqs (y)
         amps = half(abs(np.fft.fft(dataChunk)))
 
-        #freqs = freqs[10:]
-        #amps = amps[10:]
+        freqs = freqs[10:]
+        amps = amps[10:]
 
         #get top (layerNum) loudest frequencies in frame
         sortedAmps = sorted(enumerate(amps/frameRate*16/100), key=lambda x: x[1], reverse=True)[:layerNum]
         for i in range(layerNum):
             #(desmosData[0] = loudest frequency of each frame, desmosData[1] = 2nd loudest, etc)
-            desmosData[i] += "(" + str(int(freqs[sortedAmps[i][0]])) + ", " + str(round(sortedAmps[i][1], 3)) + "), "
+            desmosData[i] += f"({int(freqs[sortedAmps[i][0]])}, {sortedAmps[i][1]/10:.5f}), "
+
+            f"{i:.5f}" 
     
+    print("\n"*10)
+    print("Copy paste into desmos under data folder: \n")
 
     for i in range(layerNum):
         desmosData[i] = desmosData[i][:-2] + "]"
         print(desmosData[i])
-        #print("f(b_{" + str(i) + "})")
+    
+    print("\n(Scroll all the way up)\n")
+    input("press enter once you've pasted into desmos ")
+
+    print("\n"*10)
+    print("Copy paste these under the play folder in desmos to actually play the song: \n")
+    for i in range(layerNum):
+        print("f(b_{" + str(i) + "})")
+
+    print("\n(Scroll up a bit)")
 
 
-
+    '''
+    #graph frame
     plt.plot(freqs, amps, color="blue")
     plt.ylabel("ts amplitudes")
     plt.xlabel("ts frequencies")
     plt.title("test")
-    plt.show()
-
-    #for i in range(len(freqs)):
-    #    print(freqs[i])
+    plt.show()'''
